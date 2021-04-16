348e313b5e71fe1a8c2ae93dfacff3d3b9736571
sandeepsalkuti <salkutisandeep222@gmail.com>
2021-04-15 20:20:07 -0500

    added youtube video component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { YoutubevideosComponent } from './youtubevideos/youtubevideos.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
+  { path: 'videos', component: YoutubevideosComponent },
   // canLoad can be used only for LazyLoaded Modules
   // https://www.concretepage.com/angular/angular-canload-guard-example#CanActivate
   { path: 'training', loadChildren: './training/training.module#TrainingModule', canLoad: [AuthGuard] }
